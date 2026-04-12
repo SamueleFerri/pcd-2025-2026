05a9992e5d6a84a41e3a0ce6d21c7cecf72423db
Samuele Ferri <samuele1301@gmail.com>
2026-04-12 17:57:36 +0200

    feat: update Sketch01 to match multithreading

--- assignments/assignment-01/src/pcd/ass01/v1/Sketch01.java
+++ assignments/assignment-01/src/pcd/ass01/v1/Sketch01.java
@@ -26,52 +26,17 @@ public class Sketch01 {
 		viewModel.update(board, 0);			
 		view.render();
 		waitAbit();
+		InputThread inputThread = new InputThread(view, board);
+		BotThread botThread = new BotThread(board);
+		PhysicsThread physicsThread = new PhysicsThread(board);
+		RenderThread renderThread = new RenderThread(view, viewModel, board);
 
-		int nFrames = 0;
-		long t0 = System.currentTimeMillis();
-		long lastUpdateTime = System.currentTimeMillis();
-			
-		var pb = board.getPlayerBall();
-		var rand = new Random(2);
-		var lastKickTime = t0;
+		inputThread.start();
+		botThread.start();
+		physicsThread.start();
+		renderThread.start();
 
-		while (true){
-
-			// player input
-			Cmd cmd = view.getCmdQueue().poll();
-			while (cmd != null) {
-				cmd.execute(board);
-				cmd = view.getCmdQueue().poll();
-			}
-
-			// old random shot adapted to bot logic
-			var bb = board.getBotBall();
-			// if the ball of the bot exist, is not moving, and have passed 2 sec from the last shot
-			if (bb != null && bb.getVel().abs() < 0.05 && System.currentTimeMillis() - lastKickTime > 2000) {
-				// bot shooting in random direction (Math,PI * 2 = 360 degree)
-				var angle = rand.nextDouble() * Math.PI * 2;
-				var v = new V2d(Math.cos(angle), Math.sin(angle)).mul(1.5);
-				bb.kick(v);
-				lastKickTime = System.currentTimeMillis();
-			}
-
-			// update physics
-			long elapsed = System.currentTimeMillis() - lastUpdateTime;
-			lastUpdateTime = System.currentTimeMillis();
-			board.updateState(elapsed);
-
-			// render
-			nFrames++;
-			int framePerSec = 0;
-			long dt = (System.currentTimeMillis() - t0);
-			if (dt > 0) {
-				framePerSec = (int)(nFrames*1000/dt);
-			}
-
-			viewModel.update(board, framePerSec);
-			view.render();
-
-		}
+		System.out.println("Multithread executed");
 
 	}
 	
