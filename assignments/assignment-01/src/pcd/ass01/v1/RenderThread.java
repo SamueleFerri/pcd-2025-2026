f3a4c8fd9f27f0d8eae620c04a1f87bfacb21589
Samuele Ferri <samuele1301@gmail.com>
2026-04-12 17:40:19 +0200

    fear: add the frame calculation in RenderThread

--- assignments/assignment-01/src/pcd/ass01/v1/RenderThread.java
+++ assignments/assignment-01/src/pcd/ass01/v1/RenderThread.java
@@ -2,18 +2,35 @@ package pcd.ass01.v1;
 
 public class RenderThread extends Thread {
     private final View view;
+    private final ViewModel viewModel;
+    private final Board board;
 
-    public RenderThread(View view) {
+    public RenderThread(View view, ViewModel viewModel, Board board) {
         this.view = view;
+        this.viewModel = viewModel;
+        this.board = board;
     }
 
     @SuppressWarnings("BusyWait")
     @Override
     public void run() {
+        long t0 = System.currentTimeMillis();
+        int nFrames = 0;
+
         while (true) {
+            nFrames++;
+            int framePerSec = 0;
+            long dt = (System.currentTimeMillis() - t0);
+
+            if (dt > 0) {
+                framePerSec = (int) (nFrames * 1000 / dt);
+            }
+
+            viewModel.update(board, framePerSec);
             view.render();
+
             try {
-                Thread.sleep(10);
+                Thread.sleep(16); // ~60 FPS
             } catch (InterruptedException e) {
                 break;
             }
